681a1c29fe404da246288aa656c74ce708b5768b
David Cano <153380900+davichus1303@users.noreply.github.com>
2026-08-07 13:14:10 -0600

    imp: added catalog category to modules menu

--- src/app/features/modules/modules.service.ts
+++ src/app/features/modules/modules.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { ApiResponse } from '../../shared/interfaces/api-response.interface';
 import { Module } from '../../shared/models/module.model';
 import { BASE_URL } from '../../shared/constants/api-routes.constants';
+import { MODULES_NAVIGATION_CONSTANTS } from '../../shared/constants/modules-navigation.constants';
 
 /**
  * Service responsible for modules operations.
@@ -22,6 +24,29 @@ export class ModulesService {
    * @returns Observable with API response containing the list of modules.
    */
   getModules(): Observable<ApiResponse<Module[]>> {
-    return this.http.get<ApiResponse<Module[]>>(this.API_URL);
+    return this.http.get<ApiResponse<Module[]>>(this.API_URL).pipe(
+      map(response => {
+        if (response.data) {
+          response.data = response.data.map(module => ({
+            ...module,
+            category: this.getModuleCategory(module.link)
+          }));
+        }
+        return response;
+      })
+    );
+  }
+
+  /**
+   * Determines the category of a module based on its link.
+   *
+   * @param link Module link.
+   * @returns Module category.
+   */
+  private getModuleCategory(link: string): string | undefined {
+    if (link === '/acquisition-type-catalog') {
+      return MODULES_NAVIGATION_CONSTANTS.CATEGORIES.CATALOG;
+    }
+    return undefined;
   }
 }
